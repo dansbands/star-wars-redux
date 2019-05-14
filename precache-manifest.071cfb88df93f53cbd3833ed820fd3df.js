709bfaf79d36ac5c523e6596e50b6c255bf82206
dansbands <dansbands@hotmail.com>
2019-05-14 14:21:35 -0400

    Updates

---
+++ precache-manifest.071cfb88df93f53cbd3833ed820fd3df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed69ce14a7ba615fc89627c335ea4ee",
+    "url": "/star-wars-redux/index.html"
+  },
+  {
+    "revision": "a443c45bcfbf7f4e57be",
+    "url": "/star-wars-redux/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7021826233462cee2df6",
+    "url": "/star-wars-redux/static/css/main.0a755bce.chunk.css"
+  },
+  {
+    "revision": "a443c45bcfbf7f4e57be",
+    "url": "/star-wars-redux/static/js/2.a1b75fdb.chunk.js"
+  },
+  {
+    "revision": "7021826233462cee2df6",
+    "url": "/star-wars-redux/static/js/main.5584f9df.chunk.js"
+  },
+  {
+    "revision": "0d3aaf3c89ebb927a9db",
+    "url": "/star-wars-redux/static/js/runtime~main.57d0ab89.js"
+  },
+  {
+    "revision": "9a88c58c535ede7b03919fc8bf7388d8",
+    "url": "/star-wars-redux/static/media/bb8.9a88c58c.gif"
+  },
+  {
+    "revision": "12ba87071cbc7b5fd5a35d5ac5880a0c",
+    "url": "/star-wars-redux/static/media/logo2.12ba8707.jpg"
+  }
+]);
